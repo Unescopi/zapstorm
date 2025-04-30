3351ee6b229e08623a2713f5a6dcc3d6d227df13
Unescopi <gui10prado@hotmail.com>
2025-04-30 01:06:35 -0300

    Versao Teste

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,11 +13,6 @@ export default defineConfig({
     }
   },
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    }
+    minify: true
   }
 })
